8363146b081f3e32515a18c8ca55ff3f729238b2
totoro1809 <yossi19933@walla.co.il>
2021-02-13 13:43:09 +0200

    lab4

---
+++ work/lab 4.py
@@ -0,0 +1,52 @@
+new_list=["yosi chayun",27,"yossi19933@walla.co.il", str("050434543")]
+print(new_list)
+
+#print(new_list)
+#print(new_list[2])
+
+#new_list=[XXX,XX,XX,"xxx"]
+#rint("My age: " + str(my_list[2]))
+#my_list=[1,2,23,6.6,"dudu cohen"]
+#print("My age: " + str(my_list[2]))
+
+#y_list2=my_list*2
+#rint(my_list2)
+#לקח את כל הסטרינג הזה והפך אותו לליסט
+#my_list2=list("1234567")
+#print(my_list2)
+
+#עושה הפוך, מליסט לסטרינג עכשיו זה יהיה 1234567 במסך
+#אפשר גם להוסיף תווים בגרשיים למשל 'A' ואז יהיה 1A ו2A ו3A
+#my_string=''.join(my_list2)
+#print(my_string)
+
+#שם את הכל בתא אחד אבל רק אם אין רווחים
+#my_list3=my_string.split()
+#print(my_list3)
+
+#שם לי כל שורה בתא בפני עצמו
+#my_string="hello yarin \nhow are you?"
+#my_list3=my_string.splitlines()
+#print(my_list3)
+
+#מדפיס לי את אורך המשפט - למשל במקרה הזה מדובר כאן ב4
+my_list=["ello", 1, 66.6, "yossi"]
+#מראה לי כמה תווים יש במלל שבמרכאות
+#y_str="1232334343535346464647"
+#rint("my length is:" +str(len(my_str)))
+
+#הוספת מלל, במקרה הזה מדובר בוואו ויוסי דה קינג ופופ מבטל לי את התו האחרון במקרה הזה זה יוסי דה קינג ומשאיר רק את דודו המלך, 6 זה השורה
+#my_list.append("wow")
+#my_list.append("yossitheking")
+#print(my_list)
+#print("The length is " + str(len(my_list)))
+
+#my_list.insert(6,"dudu the king")
+#print(my_list)
+
+#my_list.pop(6)
+#print(my_list)
+
+#אמת או שקר בליסט
+my_list=["google","facebook","ebay","apple"]
+print("net4u" in my_list)
